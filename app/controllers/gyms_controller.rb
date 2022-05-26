1c0892145a89df35e911532a5868dd2d2eeeb35f
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-25 20:25:16 -0400

    basic routes all complete

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,8 +1,12 @@
 class GymsController < ApplicationController
     def show
+        gym = Gym.find_by!(id: params[:id])
+        render json: gym
     end
 
     def destroy
+        gym = Gym.find_by!(id: params[:id])
+        header :no_content
     end
 
     private
