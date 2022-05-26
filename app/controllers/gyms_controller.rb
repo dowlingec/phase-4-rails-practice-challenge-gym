126d2388fedb19d1dfc5a2887cc681ba3223250d
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-25 21:32:19 -0400

    gym not found rescue

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,4 +1,5 @@
 class GymsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
     def show
         gym = Gym.find_by!(id: params[:id])
         render json: gym
@@ -11,5 +12,10 @@ class GymsController < ApplicationController
 
     private
     def gym_params
+    end
+
+
+    def render_record_not_found
+        render json: { error: "Gym not found"}, status: 404
     end
 end
